aba88c5930fe64bdb3c0ad80413b53f49b6721d8
Alen-Berg <alexei.hlatsko@gmail.com>
2024-05-13 16:06:42 +0200

    ghp and window

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-#imports
-import pandas as pd
-import mrp
-import ghp
+import sys
+import subprocess
+from window.Window import Window
+from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 
-#code, show tables
-ghp.ghp()
-mrp.mrp('Deskorolka', 'Kola')
-mrp.mrp('Deskorolka', 'Trucki')
-mrp.mrp('Deskorolka', 'Blat')
-mrp.mrp('Blat', 'Drewno')
+def run_project():
+    process = subprocess.Popen(["python", "main.py"], stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output.decode("utf-8")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    with open('window/styles.qss', 'r', encoding="utf-8") as f:
+        style = f.read()
+        app.setStyleSheet(style)
+
+    window = Window()
+
+    run_button = QPushButton("Run Project")
+    run_button.clicked.connect(lambda: window.show_result(run_project()))
+
+    layout = QVBoxLayout()
+    layout.addWidget(run_button)
+    window.setLayout(layout)
+
+    window.show()
+    sys.exit(app.exec())
